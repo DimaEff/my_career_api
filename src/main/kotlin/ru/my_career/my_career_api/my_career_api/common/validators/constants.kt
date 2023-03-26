dfce6b5d426fa3258bb5fc8bb7624aaf832f3639
DimaEff <fominienkovd@yandex.ru>
2023-03-26 15:29:26 +0300

    added the title, description validators and constants of validators

---
+++ src/main/kotlin/ru/my_career/my_career_api/my_career_api/common/validators/constants.kt
@@ -0,0 +1,7 @@
+package ru.my_career.my_career_api.my_career_api.common.validators.constants
+
+const val STRING_MIN_SIZE = 3
+const val TITLE_MAX_SIZE = 32
+const val DESCRIPTION_MAX_SIZE = 512
+
+const val WITHOUT_SPACES_REGEX = "\\S+"
